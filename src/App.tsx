78802e97a68d76e1688fd1b287d125b51e269fad
chenshone <1351766353@qq.com>
2020-04-24 22:02:09 +0800

    button组件完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
+import Button, { ButtonSize, ButtonType } from './components/button/Button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={() => console.log('onclick')}>default</Button>
+      <Button disabled>default disabled</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        primary sm
+      </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>
+        Danger lg
+      </Button>
+      <Button
+        btnType={ButtonType.Link}
+        href="http://www.baidu.com"
+        target="_blank"
+      >
+        baidu link
+      </Button>
+      <Button disabled btnType={ButtonType.Link} href="http://www.baidu.com">
+        disabled link
+      </Button>
     </div>
   )
 }
