4d433ab44c1d5fec12c67f0077a55d98d0749e67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-26 15:16:06 +0000

    Deploy chenshone/chenshone-ui to github.com/chenshone/chenshone-ui.git:gh-pages

---
+++ precache-manifest.27dadf4f300a211d3bdbc911ee651eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009236e2336e48317e1d728621dc3167",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.61fea6d7e4558bf5ddc0.bundle.js"
+  },
+  {
+    "url": "runtime~main.61fea6d7e4558bf5ddc0.bundle.js"
+  },
+  {
+    "revision": "92173551f5a63c04353d",
+    "url": "static/css/main.cac2b19c.chunk.css"
+  },
+  {
+    "revision": "2b3bb195561e3b164157",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.61fea6d7e4558bf5ddc0.bundle.js"
+  },
+  {
+    "url": "vendors~main.61fea6d7e4558bf5ddc0.bundle.js.LICENSE.txt"
+  }
+]);
